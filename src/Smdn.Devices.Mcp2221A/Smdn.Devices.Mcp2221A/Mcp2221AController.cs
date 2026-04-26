dc647667701ea25ee8d9ff3475f9fad57e2d69a2
smdn <smdn@smdn.jp>
2026-04-26 23:03:04 +0900

    improve XML docs

--- src/Smdn.Devices.Mcp2221A/Smdn.Devices.Mcp2221A/Mcp2221AController.cs
+++ src/Smdn.Devices.Mcp2221A/Smdn.Devices.Mcp2221A/Mcp2221AController.cs
@@ -22,8 +22,17 @@ public sealed partial class Mcp2221AController :
 {
 #pragma warning restore IDE0055
   private Mcp2221ATransceiver? transceiver;
-  internal Mcp2221ATransceiver Transceiver => transceiver ?? throw new ObjectDisposedException(GetType().Name);
-  public IUsbHidDevice HidDevice => transceiver?.EndPoint?.Device ?? throw new ObjectDisposedException(GetType().Name);
+
+  internal Mcp2221ATransceiver Transceiver
+    => transceiver ?? throw new ObjectDisposedException(GetType().Name);
+
+  /// <summary>
+  /// Gets an <see cref="IUsbHidDevice"/> object representing the MCP2221/MCP2221A
+  /// as a USB HID device for controlling I2C and GPIO pins.
+  /// </summary>
+  /// <seealso cref="IUsbHidDevice"/>
+  public IUsbHidDevice HidDevice
+    => transceiver?.EndPoint?.Device ?? throw new ObjectDisposedException(GetType().Name);
 
   /// <remarks>
   /// Due to inheritance from <see cref="System.Device.Gpio.GpioDriver"/>,
@@ -35,6 +44,19 @@ public sealed partial class Mcp2221AController :
   private readonly Mcp2221AGpioDriver gpioDriver;
 #pragma warning restore CA2213
 
+  /// <summary>
+  /// Gets a <see cref="Mcp2221AI2cBus"/> that operates the I2C peripherals
+  /// using the current <see cref="Mcp2221AController"/> instance as the
+  /// underlying <see cref="System.Device.I2c.I2cBus"/>.
+  /// </summary>
+  /// <remarks>
+  /// Since this property can be used as <see cref="System.Device.I2c.I2cBus"/>,
+  /// you can use this object to integrate with the I2C device bindings
+  /// provided by <see href="https://learn.microsoft.com/dotnet/iot/intro">Iot.Device.Bindings</see>.
+  /// </remarks>
+  /// <seealso cref="System.Device.I2c.I2cBus"/>
+  /// <seealso cref="II2cController"/>
+  /// <seealso cref="II2cControllerExtensions"/>
   [CLSCompliant(false)]
   public Mcp2221AI2cBus I2cBus {
     get {
@@ -43,6 +65,17 @@ public sealed partial class Mcp2221AController :
     }
   }
 
+  /// <summary>
+  /// Gets an <see cref="IGpControllerGroup"/> object that implements the GPIO
+  /// functionality of the MCP2221/MCP2221A, providing bulk control of GPIO pins
+  /// (<c>GP0</c>-<c>GP3</c>) with a single command.
+  /// </summary>
+  /// <seealso cref="GpPin0"/>
+  /// <seealso cref="GpPin1"/>
+  /// <seealso cref="GpPin2"/>
+  /// <seealso cref="GpPin3"/>
+  /// <seealso cref="IGpControllerGroup"/>
+  /// <seealso cref="IGpControllerGroupExtensions"/>
   [CLSCompliant(false)]
   public IGpControllerGroup GpPins {
     get {
@@ -51,9 +84,28 @@ public sealed partial class Mcp2221AController :
     }
   }
 
+  /// <summary>
+  /// Gets a <see cref="Gp0Controller"/> to configure and control the GPIO functionality
+  /// available on the <c>GP0</c> pin of the MCP2221/MCP2221A.
+  /// </summary>
   public Gp0Controller GpPin0 => GpPins.Gp0;
+
+  /// <summary>
+  /// Gets a <see cref="Gp1Controller"/> to configure and control the GPIO functionality
+  /// available on the <c>GP1</c> pin of the MCP2221/MCP2221A.
+  /// </summary>
   public Gp1Controller GpPin1 => GpPins.Gp1;
+
+  /// <summary>
+  /// Gets a <see cref="Gp2Controller"/> to configure and control the GPIO functionality
+  /// available on the <c>GP2</c> pin of the MCP2221/MCP2221A.
+  /// </summary>
   public Gp2Controller GpPin2 => GpPins.Gp2;
+
+  /// <summary>
+  /// Gets a <see cref="Gp3Controller"/> to configure and control the GPIO functionality
+  /// available on the <c>GP3</c> pin of the MCP2221/MCP2221A.
+  /// </summary>
   public Gp3Controller GpPin3 => GpPins.Gp3;
 
   /// <summary>
@@ -68,7 +120,7 @@ public sealed partial class Mcp2221AController :
   /// </para>
   /// <para>
   /// It is recommended that when passing an instance of this property as a <see cref="GpioController"/>
-  /// to device binding classes such as <see href="https://www.nuget.org/packages/Iot.Device.Bindings">Iot.Device.Bindings</see>,
+  /// to device binding classes such as <see href="https://learn.microsoft.com/dotnet/iot/intro">Iot.Device.Bindings</see>,
   /// set the <c>shouldDispose</c> parameter to <see langword="false"/> and manage the lifecycle of
   /// the <see cref="Mcp2221AController"/> instance separately.
   /// </para>
@@ -86,8 +138,8 @@ public sealed partial class Mcp2221AController :
   /// </summary>
   /// <remarks>
   /// This property represents the global configuration for the DAC module
-  /// of the MCP2221A. Changing the reference source on one GP pin will
-  /// affect all other GP pins configured as DAC outputs.
+  /// of the MCP2221/MCP2221A. Changing the reference source on one GP pin
+  /// will affect all other GP pins configured as DAC outputs.
   /// </remarks>
   /// <seealso cref="Gp2Controller.ConfigureAsDac"/>
   /// <seealso cref="Gp3Controller.ConfigureAsDac"/>
@@ -105,8 +157,8 @@ public sealed partial class Mcp2221AController :
   /// </summary>
   /// <remarks>
   /// This property represents the global configuration for the DAC module
-  /// of the MCP2221A. If no write operation has been performed yet, this
-  /// property returns the value currently held by the controller (e.g.,
+  /// of the MCP2221/MCP2221A. If no write operation has been performed yet,
+  /// this property returns the value currently held by the controller (e.g.,
   /// the default value from Flash settings).
   /// </remarks>
   /// <seealso cref="Gp2Controller.ConfigureAsDac"/>
@@ -124,8 +176,8 @@ public sealed partial class Mcp2221AController :
   /// </summary>
   /// <remarks>
   /// This property represents the global configuration for the ADC module
-  /// of the MCP2221A. Changing the reference source on one GP pin will
-  /// affect all other GP pins configured as ADC inputs.
+  /// of the MCP2221/MCP2221A. Changing the reference source on one GP pin
+  /// will affect all other GP pins configured as ADC inputs.
   /// </remarks>
   /// <seealso cref="Gp1Controller.ConfigureAsAdc"/>
   /// <seealso cref="Gp2Controller.ConfigureAsAdc"/>
