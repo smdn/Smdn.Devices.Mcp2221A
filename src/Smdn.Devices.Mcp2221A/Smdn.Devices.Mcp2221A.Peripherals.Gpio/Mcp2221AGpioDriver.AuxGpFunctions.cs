581443e49ec6d961aedb31b2b592987fc1e9b127
smdn <smdn@smdn.jp>
2026-05-04 21:01:56 +0900

    merge implementation for auxiliary GP functions into single file

--- src/Smdn.Devices.Mcp2221A/Smdn.Devices.Mcp2221A.Peripherals.Gpio/Mcp2221AGpioDriver.AuxGpFunctions.cs
+++ src/Smdn.Devices.Mcp2221A/Smdn.Devices.Mcp2221A.Peripherals.Gpio/Mcp2221AGpioDriver.AuxGpFunctions.cs
@@ -73,6 +73,24 @@ partial class Mcp2221AGpioDriver {
       ? VoltageReferenceSource.Vdd
       : (VoltageReferenceSource)voltageReferenceBits;
 
+  public InterruptOnChangeTrigger CurrentInterruptOnChangeTrigger {
+    get {
+      var iocSetup = sramSettings.ReadInterruptDetectionModuleSetupByte();
+
+      return
+        ((iocSetup & 0b_0_00_0_1_0_0_0) == 0 ? InterruptOnChangeTrigger.None : InterruptOnChangeTrigger.Rising) |
+        ((iocSetup & 0b_0_00_0_0_0_1_0) == 0 ? InterruptOnChangeTrigger.None : InterruptOnChangeTrigger.Falling);
+    }
+  }
+
+  public bool LastFetchedInterruptDetectionFlag { get; private set; }
+
+  public ClockOutputFrequency CurrentClockOutputFrequency
+    => (ClockOutputFrequency)(sramSettings.ReadClockOutputDividerValueByte() & 0b_0_00_00_111);
+
+  public ClockOutputDutyCycle CurrentClockOutputDutyCycle
+    => (ClockOutputDutyCycle)((sramSettings.ReadClockOutputDividerValueByte() & 0b_0_00_11_000) >> 3);
+
   private static class FetchGpPinInputsCommand {
 #pragma warning disable SA1313
     public static void ConstructCommand(
@@ -225,5 +243,23 @@ partial class Mcp2221AGpioDriver {
     await FetchGpPinInputsAsync(cancellationToken).ConfigureAwait(false);
 
     return lastFetchedAdcSample;
+  }
+
+  public bool FetchInterruptDetectionFlag(
+    CancellationToken cancellationToken
+  )
+  {
+    FetchGpPinInputs(cancellationToken);
+
+    return LastFetchedInterruptDetectionFlag;
+  }
+
+  public async ValueTask<bool> FetchInterruptDetectionFlagAsync(
+    CancellationToken cancellationToken
+  )
+  {
+    await FetchGpPinInputsAsync(cancellationToken).ConfigureAwait(false);
+
+    return LastFetchedInterruptDetectionFlag;
   }
 }
